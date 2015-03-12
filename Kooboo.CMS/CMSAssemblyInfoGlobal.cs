2cdd36e25b815c98f126829a023d9ba22262427b
Tsingbo-Kooboo <qingbo@kooboo.com>
2015-03-12 09:33:37 +0800

    support mvc5

--- Kooboo.CMS/CMSAssemblyInfoGlobal.cs
+++ Kooboo.CMS/CMSAssemblyInfoGlobal.cs
@@ -12,5 +12,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Yari 2009-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("4.4.0.0203")]
-[assembly: AssemblyFileVersion("4.4.0.0203")]
+[assembly: AssemblyVersion("4.4.0.0312")]
+[assembly: AssemblyFileVersion("4.4.0.0312")]
